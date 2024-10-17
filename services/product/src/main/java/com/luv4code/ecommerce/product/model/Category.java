fd44e51a3eb8274f06cedc91ea1b8ca1c2f290e7
Wentworth@34208 <Madhav.p.akv@gmail.com>
2024-10-17 17:42:35 +0530

    Adding Entities and Flyway db migration is created.

---
+++ services/product/src/main/java/com/luv4code/ecommerce/product/model/Category.java
@@ -0,0 +1,23 @@
+package com.luv4code.ecommerce.product.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
